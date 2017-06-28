a9a12060d84016658c45ff30f237d46a17625da4
taracaca <fernando_levy_assis@hotmail.com>
2017-06-28 13:26:09 -0300

    primeiro

---
+++ src/main/java/Pratica.java
@@ -0,0 +1,71 @@
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import utfpr.ct.dainf.if62c.avaliacao.Lancamento;
+import utfpr.ct.dainf.if62c.avaliacao.ProcessaLancamentos;
+
+/**
+ * IF62C Fundamentos de Programação 2
+ * Avaliação parcial.
+ * @author 
+ */
+public class Pratica {
+    
+    static int conta;
+    static List<Lancamento> listlancamento = new ArrayList<>();
+ 
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        Scanner scanner = new Scanner(System.in); 
+        ProcessaLancamentos processor;
+        
+        System.out.println("Caminho");
+        String caminho = scanner.next();
+        
+        processor = new ProcessaLancamentos(caminho);
+        listlancamento = processor.getLancamentos();
+        
+        System.out.println("Conta");
+            
+        while(scanner.hasNextInt()){
+            
+            conta = scanner.nextInt();
+            
+            if(conta != 0){
+                Pratica.exibeLancamentosConta(listlancamento, conta);
+            }
+            
+            else{
+                System.exit(0);
+                }
+            }
+    }
+    
+    public static void exibeLancamentosConta(List<Lancamento> lancamentos, Integer conta) {
+        
+        List<Lancamento> listconta = new ArrayList<>();
+        Lancamento temp = new Lancamento(conta, null, null, null);
+        boolean ver = true;
+        
+        int indice = lancamentos.indexOf(temp);
+        
+        if(indice >= 0){
+            listconta.add(lancamentos.get(indice));
+            indice++;
+            while(lancamentos.get(indice).getConta() ==  lancamentos.get(indice-1).getConta()){
+                listconta.add(lancamentos.get(indice));
+                indice++;
+            }
+            for(Lancamento l: listconta){
+                        System.out.println(l);
+                    }
+       
+        }    
+        else System.out.println("Conta inexistente.");
+        
+    }
+    
+}
---
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/ProcessaLancamentos.java
@@ -0,0 +1,83 @@
+package utfpr.ct.dainf.if62c.avaliacao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * IF62C Fundamentos de Programação 2
+ * Avaliação parcial.
+ * @author 
+ */
+public class ProcessaLancamentos {
+    
+    private BufferedReader reader;
+    private List<Lancamento> lancamentos;
+
+    public ProcessaLancamentos(File arquivo) throws FileNotFoundException {
+        
+        reader = new BufferedReader(new FileReader(arquivo));
+    }
+
+    public ProcessaLancamentos(String path) throws FileNotFoundException {
+        
+        reader = new BufferedReader(new FileReader(new File(path)));
+    }
+    
+    private String getNextLine() throws IOException {
+        
+        return reader.readLine();
+    }
+    
+    private Lancamento processaLinha(String linha) {
+        
+        Integer ano, mes, dia, conta;
+        double valor;
+        String descricao;
+        
+        conta = Integer.parseInt(linha.substring(0, 6));
+        dia = Integer.parseInt(linha.substring(12, 14));
+        mes = Integer.parseInt(linha.substring(10, 12));
+        ano = Integer.parseInt(linha.substring(6, 10));
+        descricao = linha.substring(14, 74).trim();
+        valor = Long.valueOf(linha.substring(74, 86))/100;
+        
+        GregorianCalendar data = new GregorianCalendar(ano, mes-1, dia);
+        Date date = data.getTime();
+        
+        return new Lancamento(conta, date, descricao, valor);
+    }
+    
+    private Lancamento getNextLancamento() throws IOException {
+        
+        String linha = this.getNextLine();
+        
+        if(linha != null){
+            return this.processaLinha(linha);
+        }
+        else return null;
+    }
+    
+    public List<Lancamento> getLancamentos() throws IOException {
+        
+        Lancamento atual;
+        lancamentos = new ArrayList<>();
+        
+        while((atual = getNextLancamento()) != null){
+            lancamentos.add(atual);
+        }
+        reader.close();
+        
+        Collections.sort(lancamentos, new LancamentoComparator());
+        
+        return lancamentos;
+    }
+
+}
